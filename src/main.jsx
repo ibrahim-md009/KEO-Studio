ff185e560e5e874ca083bad850c309b1630d318f
ibrahim-md009 <ibrahim.shubair.dev@gmail.com>
2026-09-24 02:11:13 +0300

    delete question from faq

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 );
